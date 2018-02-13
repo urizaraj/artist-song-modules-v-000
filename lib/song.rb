3517a2ed0afca2b18b8b1b210d2ea63227f78c2b
Alexander Urizar <urizaraj@gmail.com>
2018-02-13 00:21:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,9 +19,9 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 
   def self.all
     @@songs
